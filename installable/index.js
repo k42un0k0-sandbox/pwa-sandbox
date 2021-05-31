5e86f116409c5e2870aa51dd2bbee6848242b2bc
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-31 22:04:56 +0900

    minimum

--- installable/index.js
+++ installable/index.js
@@ -1,17 +1,3 @@
 (function () {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("./sw.js")
-      .then(function (registration) {
-        // 登録成功
-        console.log(
-          "ServiceWorker の登録に成功しました。スコープ: ",
-          registration.scope
-        );
-      })
-      .catch(function (err) {
-        // 登録失敗
-        console.log("ServiceWorker の登録に失敗しました。", err);
-      });
-  }
+  navigator.serviceWorker.register("./sw.js");
 })();
